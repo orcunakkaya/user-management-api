9045a1e57a79cba9227924810182bf12eb04d8f5
orcunakkaya <berkayakkaya49@gmail.com>
2026-04-29 19:26:32 +0300

    add user controller with basic CRUD endpoints

---
+++ src/main/java/com/orcunakkaya/usermanagementapi/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.orcunakkaya.usermanagementapi.controller;
+
+import com.orcunakkaya.usermanagementapi.entity.User;
+import com.orcunakkaya.usermanagementapi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
---
+++ src/main/java/com/orcunakkaya/usermanagementapi/service/UserService.java
@@ -0,0 +1,30 @@
+package com.orcunakkaya.usermanagementapi.service;
+
+import com.orcunakkaya.usermanagementapi.entity.User;
+import com.orcunakkaya.usermanagementapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
