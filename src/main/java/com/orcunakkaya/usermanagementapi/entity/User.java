7bf33fec357851ac49332feb646a6a8ed2879690
orcunakkaya <berkayakkaya49@gmail.com>
2026-04-29 19:07:36 +0300

    init spring boot project structure

---
+++ src/main/java/com/orcunakkaya/usermanagementapi/entity/User.java
@@ -0,0 +1,28 @@
+package com.orcunakkaya.usermanagementapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private Integer age;
+}
